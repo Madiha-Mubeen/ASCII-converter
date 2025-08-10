51a743a5ee56a9342bda75080a8c53333266005a
Madiha <madihamubeenhawaldar@gmail.com>
2025-08-10 18:59:00 +0530

    trying to add matrix rain

--- ascii.js
+++ ascii.js
@@ -142,3 +142,49 @@ toggleCheckbox.addEventListener("change", () => {
         sound.play().catch(err => console.warn("OOPS! Sound play error:", err));
     }
 });
+/*
+//matrix rain
+const canvas = document.getElementById("matrixCanvas");
+const ctx = canvas.getContext("2d");
+
+//Full Screen
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+//  Characters
+const chars = "0.1".split("");
+const fontSize = 15;
+const columns = canvas.Width / fontSize;
+
+//Array for rain drops
+let drops = [];
+for (let i = 0; i < columns; i++) {
+    drops[i] = 1;
+}
+
+//Draw function
+function drawMatrix() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillRect(0,0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#0f0";
+    ctx.font = fontSize + "px monospace";
+
+    for (let i = 0; i < drops.length; i++) {
+        const text = chars[Math.floor(Math.random() * chars.length)];
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+        if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+            drops[i] = 0;
+        }
+        drops[i]++;
+    }
+}
+
+setInterval(drawMatrix, 50);
+
+//Adjust on resize
+window.addEventListener("resize", () => {
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+}); */
